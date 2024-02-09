f518ae569c2665f5c389ba065106995e115f6b49
Helix1579 <mashruwalayashraj9@gmail.com>
2024-02-09 11:49:54 +0530

    Header Complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from './Pages/SignIn';
 import SignOut from './Pages/SignOut';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
+import Header from './Components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
