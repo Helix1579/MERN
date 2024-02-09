65ddd61ed7cf331e266e0142c2a04353e2c71d1d
Helix1579 <mashruwalayashraj9@gmail.com>
2024-02-09 15:43:16 +0530

    Test API

--- API/index.js
+++ API/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./Routes/UserRoute.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(() => {
@@ -14,3 +16,13 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000 :)");
 });
+
+app.get("/test", (req, res) => {
+  res.json(
+    {
+      message: "Hello World !"
+    }
+  );
+});
+
+app.use("/api/users", userRoute);
