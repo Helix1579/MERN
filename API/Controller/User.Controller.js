eefc2a95f29c7211e15ac074b105d7de1ea5ea1b
Helix1579 <mashruwalayashraj9@gmail.com>
2024-02-15 17:34:32 +0530

    Update User (In Progress)

--- API/Controller/User.Controller.js
+++ API/Controller/User.Controller.js
@@ -1,3 +1,7 @@
+import User from "../Models/UserModel.js";
+import { errorHandler } from "../Utilities/error.js";
+import bcrypt from "bcryptjs";
+
 export const test = (req, res) => {
     res.json(
         {
@@ -5,3 +9,30 @@ export const test = (req, res) => {
         }
     );
 };
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "Not Authorized"))
+    }
+    try {
+        if (req.body.password) {
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, { new: true });
+
+        // const { password, ...rest } = updatedUser._doc;
+
+        res.status(200).json(updatedUser)
+
+    } catch (error) {
+        next(error)
+    }
+}
--- API/Routes/UserRoute.js
+++ API/Routes/UserRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { test } from "../Controller/User.Controller.js";
+import { test, updateUser } from "../Controller/User.Controller.js";
+import { verifyToken } from "../Utilities/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.get("/update/:id",verifyToken ,updateUser);
 
 export default router;
