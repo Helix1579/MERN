71283d16dac28543635229009d7d744b2486e884
Helix1579 <mashruwalayashraj9@gmail.com>
2024-02-28 11:35:16 +0530

    API created

--- API/Routes/ListingRoute.js
+++ API/Routes/ListingRoute.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createListing, deleteListing, updateListing, getListing } from "../Controller/Listing.Controller.js";
+import { 
+    createListing,
+    deleteListing,
+    updateListing, 
+    getListing, 
+    getListings, 
+} from "../Controller/Listing.Controller.js";
 import { verifyToken } from "../Utilities/verifyUser.js";
 
 const router = express.Router();
@@ -8,5 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.patch("/update/:id", verifyToken, updateListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
